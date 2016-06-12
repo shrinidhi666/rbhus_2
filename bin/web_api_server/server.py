2350b26c0706e4d5d7228d69fa3f8dcbc6fefcd2
Shrinidhi Rao K <shrinidhi666@gmail.com>
2016-06-12 11:34:17 +0530

    protoyping

--- bin/web_api_server/server.py
+++ bin/web_api_server/server.py
@@ -1,6 +1,8 @@
 import setproctitle
 import sys
 import cherrypy
+import psutil
+import simplejson
 
 setproctitle.setproctitle("web_api_server")
 
@@ -9,6 +11,12 @@ class HelloWorld(object):
   @cherrypy.expose
   def index(self):
     return "Hello world!"
+
+  @cherrypy.expose
+  def get_node_info(self, **kwargs):
+    cpu_stats = psutil.cpu_stats()
+    print (cpu_stats)
+
 
 
 if __name__ == '__main__':
