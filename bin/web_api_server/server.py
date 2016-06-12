e8e83956edac774c5a4b05ea225e98528460e0fd
Shrinidhi Rao K <shrinidhi666@gmail.com>
2016-06-12 12:41:38 +0530

    protoyping

--- bin/web_api_server/server.py
+++ bin/web_api_server/server.py
@@ -9,13 +9,18 @@ setproctitle.setproctitle("web_api_server")
 
 class HelloWorld(object):
   @cherrypy.expose
-  def index(self):
-    return "Hello world!"
+  def ping(self):
+    return "alive"
 
   @cherrypy.expose
   def get_node_info(self, **kwargs):
-    cpu_stats = psutil.cpu_stats()
-    print (cpu_stats)
+    node_info = {}
+    node_info['cpu_count'] = psutil.cpu_count()
+    if(sys.platform.lower().find('linux') >= 0):
+      node_info['loadavg'] = open('/proc/loadavg','r').read().split()
+    node_info['cpu_percent'] = psutil.cpu_percent(interval=1)
+
+    return (simplejson.dumps(node_info))
 
 
 
