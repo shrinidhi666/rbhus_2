82d4279d4cffa0d9e5bbea9660e2b6f00e3f60db
Shrinidhi Rao K <shrinidhi666@gmail.com>
2016-07-01 21:18:08 +0530

    protoyping

--- lib/db/rbhus_infra.py
+++ lib/db/rbhus_infra.py
@@ -4,14 +4,11 @@ __author__ = "Shrinidhi Rao"
 __license__ = "GPL"
 __email__ = "shrinidhi666@gmail.com"
 
-
-import sys
 import os
-sys.path.append(os.sep.join(os.path.abspath(__file__).split(os.sep)[:-3]))
-import time
+import sys
 
-import psycopg2
-import psycopg2.extras
+sys.path.append(os.sep.join(os.path.abspath(__file__).split(os.sep)[:-3]))
+
 import lib.db.base
 
 
