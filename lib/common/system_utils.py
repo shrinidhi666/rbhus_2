a316c03c6ff8ffcf38009d9ddfa381537bd50c9b
Shrinidhi Rao K <shrinidhi666@gmail.com>
2016-07-23 20:14:39 +0530

    protoyping

--- lib/common/system_utils.py
+++ lib/common/system_utils.py
@@ -14,6 +14,7 @@ import simplejson
 import psutil
 import socket
 import multiprocessing
+import logging
 
 
 
@@ -49,8 +50,49 @@ class local_host(object):
 
   @property
   def cpu_used(self):
+    cpu_used_row = self.db_queue.execute("select cpu_used from host_details where host_ip=%s",(self.host_ip,),dictionary=True)
+    if(not isinstance(cpu_used_row,int)):
+      return (cpu_used_row[0]['cpu_used'])
+    else:
+      return (0)
 
-    self.db_queue.execute()
+  @cpu_used.setter
+  def cpu_used(self,value):
+    cpu_free = self.cpu_total - value
+    if(cpu_free >= 0 and cpu_free <= self.cpu_total):
+      self.db_queue.execute("update host_details set cpu_used=%s where host_ip=%s",(value,self.host_ip,))
+    else:
+      logging.error("not a valid cpu_used value")
+
+  @property
+  def loadavg(self):
+    if (sys.platform.lower().find("linux") >= 0):
+      return (os.getloadavg()[0])
+    elif (sys.platform.lower().find("win") >= 0):
+      return (psutil.cpu_percent(interval=1))
+
+  @loadavg.setter
+  def loadavg(self,value):
+    pass
+
+  @property
+  def is_enabled(self):
+    is_enabled_row = self.db_queue.execute("select is_enabled from host_details where host_ip=%s",(self.host_ip,),dictionary=True)
+    if(not isinstance(is_enabled_row,int)):
+      return (is_enabled_row[0]['is_enabled'])
+    else:
+      logging.error("no valid host_ip found")
+    return (False)
+
+  @is_enabled.setter
+  def is_enabled(self,value):
+    if(isinstance(value,bool)):
+      self.db_queue.execute("update host_details set is_enabled=%s where host_ip=%s",(value,self.host_ip,))
+    else:
+      logging.error("boolean expected: True/False")
+
+
+
 
 
 
@@ -73,6 +115,7 @@ def get_local_host_details():
   details['disk'] = psutil.disk_partitions()
   details['cpu_total'] = multiprocessing.cpu_count()
 
+
   return(details)
 
 
@@ -80,3 +123,9 @@ if(__name__ == '__main__'):
   local_dets = local_host()
   local_dets.host_ip = "test"
   print(local_dets.host_ip)
+  print (local_dets.cpu_used)
+  print(local_dets.loadavg)
+  local_dets.cpu_used = 2
+  print (local_dets.cpu_used)
+  print (local_dets.is_enabled)
+  local_dets.is_enabled = 0
